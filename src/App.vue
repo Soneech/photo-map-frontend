702b88a09e1988098049f20871d2422d3310f6c6
Soneech <sonya.patrina@yandex.ru>
2025-03-29 13:29:24 +0300

    Trying to add Yandex maps API...

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,4 @@
             </div>
         </footer>
     </div>
-    
-    
 </template>
---
+++ src/lib/ymaps.ts
@@ -0,0 +1,30 @@
+import * as Vue from 'vue';
+
+
+function waitForYmaps3(): Promise<any> {
+  return new Promise((resolve) => {
+    const check = () => {
+      if ((window as any).ymaps3) {
+        resolve((window as any).ymaps3);
+      } else {
+        setTimeout(check, 50);
+      }
+    };
+    check();
+  });
+}
+
+const ymaps3 = await waitForYmaps3();
+const [ymaps3Vue] = await Promise.all([
+  ymaps3.import('@yandex/ymaps3-vuefy'),
+  ymaps3.ready
+]);
+
+export const vuefy = ymaps3Vue.vuefy.bindTo(Vue);
+export const {
+  YMap,
+  YMapDefaultSchemeLayer,
+  YMapDefaultFeaturesLayer,
+  YMapMarker
+} = vuefy.module(ymaps3);
+
