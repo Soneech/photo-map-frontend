244725527fae5e492c6571c173b0179acc58c09a
Soneech <sonya.patrina@yandex.ru>
2025-03-29 15:04:27 +0300

    Add classic ya-maps-api settings for vue.js

---
+++ src/components/YMapContainer.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+    import {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker} from '../lib/ymaps';
+    import type {YMapLocationRequest} from 'ymaps3';
+
+    const LOCATION: YMapLocationRequest = {
+        center: [37.588144, 55.733842],
+        zoom: 9
+    };
+</script>
+
+<template>
+    <div class="map-container" style="width: 90%; height: 78vh; margin: auto;">
+        <YMap :location="LOCATION">
+
+        <YMapDefaultSchemeLayer />
+        <YMapDefaultFeaturesLayer />
+
+        </YMap>
+    </div>
+</template>
